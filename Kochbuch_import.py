24540b521e73d12b2b5b4288824285c47e55cfee
Angelika Diehl <68091502+TruddelDu@users.noreply.github.com>
2022-02-16 18:01:53 +0100

    ingredients import + log file
    finished the import functions for ingredients and enabled log-files

--- Kochbuch_import.py
+++ Kochbuch_import.py
@@ -1,84 +1,57 @@
 import my_functions as mf
 import pandas as pd
+import logging
+import sys
+import time
 
-path = r"D:\Benutzer\Angelika\Dokumente\DataScienceProjects\02_Kochbuch\Kochbuch\\"
-# First find the files of all recipes
-recipe_files = mf.find_recipe_files(f'{path}uebersicht.tex') 
 
-# then extract the ingredients
-replace_in_file = {'\\\\':'', '\\textonehalf':'0.5', '\\textonequarter':'0.25', 'Hand voll':'Handvoll'}
-units = ['Tasse', 'Tassen', 'ml', 'mL', 'L', 'l', 'g', 'TL', 'EL', 'Dose', 'Prise', 'Stück', 'Spritzer', 'Handvoll']
-with open(f'{path}{recipe_files[38]}.tex', encoding='utf-8') as recipe:
-    # import latex and replace some annoying things
-    recipe_text = recipe.read()
-    for annoying, better in replace_in_file.items():
-        recipe_text = recipe_text.replace(annoying, better)
+def main():
+    path = r"D:\Benutzer\Angelika\Dokumente\DataScienceProjects\02_Kochbuch\Kochbuch\\"
+    # First find the files of all recipes
+    recipe_files = mf.find_recipe_files(f'{path}uebersicht.tex') 
 
-    # extract recipe name
-    recipe_title = recipe_text[:recipe_text.find('}')].split('{')[-1]
-    print(recipe_title)
+    # then extract the ingredients
+    replace_in_file = {'\\\\':'', '\\textonehalf':'0.5', '\\textonequarter':'0.25', 'Hand voll':'Handvoll'}
+    ingredients = [['Rezept', 'Zutat', 'Teil des Rezepts', 'Menge', 'Einheit',]]
+    for recipe_file in recipe_files:
+        with open(f'{path}{recipe_file}.tex', encoding='utf-8') as recipe:
+            # import latex and replace some annoying things
+            recipe_text = recipe.read()
+            for annoying, better in replace_in_file.items():
+                recipe_text = recipe_text.replace(annoying, better)
 
-    # extract ingredients
-    ingredients = [['Zutat', 'Teil des Rezepts', 'Menge', 'Einheit',]]
-    recipe_ingredients_list = mf.get_ingredients_list(recipe_text)
-    ingredient_section = ''
-    for ingredient in recipe_ingredients_list:
-        ingredient = ingredient.strip('\t')
-        # sometimes the ingredients are not given for the enitre recipe but for sections
-        if 'multicolumn' in ingredient:
-            ingredient_section = ingredient.strip('}:\\')[ingredient.rfind('{'):]
-            continue
+            # extract recipe name
+            recipe_title = recipe_text[:recipe_text.find('}')].split('{')[-1]
+            
 
-        ing_name = ingredient.split('&')[-1]
-        ing_amount = ingredient.split('&')[0]
+            # extract ingredients
+            ingredients.extend(mf.extract_ingredients(recipe_text, recipe_title))
 
-        #clean-up of ingredient name
-        if 'textsuperscript' in ing_name:
-            ing_name = ing_name[:ing_name.find('\\textsuperscript')]
+            logger.info(f'import of {recipe_title} finished')
 
-        current_ingredient = [ing_name]
-        current_ingredient.append(ingredient_section)
-        
-        #do not add empty rows to the database
-        if ing_name=='':
-            continue
-        # the ingredient name can extend to the next row, at least sometimes signified by starting with 'oder '
-        elif ing_name[:5]=='oder ':
-            ingredients[-1][0] = f'{ingredients[-1][0]} {ing_name}' # this 2nd part of the ingredient name is added to the correct place
-            continue
-        # some recipes do not have any amount and are given in just one column
-        elif ing_amount==ing_name:
-            current_ingredient.append(float('NaN')) # amount
-            current_ingredient.append('') # unit
-        # some ingredients are not given with an amount
-        elif ing_amount=='' or ing_amount==' ':
-            current_ingredient.append(float('NaN')) #amount
-            current_ingredient.append('') #unit
-        # due to spacing issues the unit is sometimes in the 2nd column
-        elif '\\,' in ing_amount: # the non-breaking space signifies that the unit is in the first column with the amount
-            current_ingredient.append(mf.extract_ingredient_amount(ing_amount.split('\\,')[0])) # amount
-            current_ingredient.append(ing_amount.split('\\,')[1].strip(' ')) #unit
-        # sometimes the unit is in the 1st column but no non-breaking space was used, here any non-numeric character is recognized as a unit
-        elif not ing_amount.isnumeric():
-            if ing_amount.split(' ')[0].isnumeric():
-                current_ingredient.append(mf.extract_ingredient_amount(ing_amount.split(' ')[0])) #amount
-            else:
-                current_ingredient.append(1) #unit
-                print(f'Check if this has acutally the amount of 1: {ingredient}')
-            # sometimes more information ( as in 'kl. Dose') can be found
-            ing_unit = []
-            for el in ing_amount.split(' '):
-                if not el.isnumeric():
-                    ing_unit.append(el)
-            current_ingredient.append(' '.join(ing_unit)) #unit
-        # if the 1st column does not contain a unit, and the 2nd also does not contain a unit
-        elif ing_name.split(' ')[0] not in units:
-            current_ingredient.append(mf.extract_ingredient_amount(ing_amount)) #amount
-            current_ingredient.append('') #unit
-        else:
-            print('Nicht abgefangen')
-            print(ingredient)
-
-        ingredients.append(current_ingredient)
-    
     print(pd.DataFrame(ingredients[1:], columns=ingredients[0]))
+
+
+if __name__ == '__main__':
+    ## configuration of logger
+    logging.basicConfig(stream=sys.stdout, level=logging.INFO)
+    
+    logger = logging.getLogger(__name__)
+    logger.setLevel(logging.INFO)
+
+    formatter = logging.Formatter('%(asctime)s:%(levelname)s:%(message)s')
+
+    file_handler = logging.FileHandler('import.log')
+    file_handler.setFormatter(formatter)
+
+    stream_handler = logging.StreamHandler()
+    stream_handler.setFormatter(formatter)
+
+    logger.addHandler(file_handler)
+    logger.addHandler(stream_handler)
+
+    logger.info(f'start @ {time.strftime("%Y-%m-%d %H:%M:%S", time.localtime())}')
+    start = time.perf_counter()
+    main()
+    end = time.perf_counter()
+    logger.info(f'runtime: {round((end-start)/60,1)} m')
--- my_functions.py
+++ my_functions.py
@@ -1,11 +1,19 @@
 import numpy as np
 from re import split as resplit
+import logging
 
+logger = logging.getLogger(__name__)
 
 def find_recipe_files(path):
-    ''' Returns a list of all file names of receipts contained in the file. Recognizes these file name by an '\input' statement.
-    path: string, path to the tex-file containing the overview over all recipes
-    '''
+    """Returns a list of all file names of receipts contained in the file. 
+    Recognizes these file name by an '\input' statement.
+
+    Args:
+        path (sting): path to the tex-file containing the overview over all recipes
+
+    Returns:
+        list: filenames of the recipes
+    """
     recipe_files = list()
     with open(path,'r', encoding='utf-8') as ubersicht:
         for line in ubersicht.readlines():
@@ -14,18 +22,44 @@ def find_recipe_files(path):
     return recipe_files
 
 def get_ingredients_list(recipe_text):
-    ''' Returns the ingredients as a list.
-    recipe_text: string, latex code of recipe
-    '''
+    """Returns the ingredients as a list.
+
+    Args:
+        recipe_text (sting): latex code of recipe
+
+    Returns:
+        list: ingredients in a list
+    """
     #find the table and from there the next newline
     ingredients_start = recipe_text.find('begin{tabular}{l') + recipe_text[recipe_text.find('begin{tabular}{l')+1:].find('\n') + 1
     ingredients_end = recipe_text.find('end{tabular}')-1 # find the end of the table
     return recipe_text[ingredients_start:ingredients_end].split('\n')
 
+def is_string_float(potential_float):
+    """Returns True if the given string can be formatted into a float. Excepts dots and commas as decimal separators.
+
+    Args:
+        potential_float (string): the string that might be a float
+
+    Returns:
+        boolean: wether or not potential_float is an acutal float
+    """
+    potential_float = potential_float.replace(',','.')
+    try:
+        float(potential_float)
+        return True
+    except ValueError:
+        return False
+
 def extract_ingredient_amount(ing_amount):
-    ''' Returns the amount of an ingredient as a float.
-    ing_amount: string, 1st column in Ingredients list
-    '''
+    """Returns the amount of an ingredient as a float.
+
+    Args:
+        ing_amount (sting):  1st column in Ingredients list
+
+    Returns:
+        float: amount of the ingredient
+    """
     # at times a german decimal (,) was used instead of the english (.)
     ing_amount = ing_amount.replace(',','.')
     ing_amount = ing_amount.replace('$\\sim$', '') 
@@ -44,3 +78,92 @@ def extract_ingredient_amount(ing_amount):
     #     return float('NaN')
     else:
         return float(ing_amount.split('\\,')[0].replace(',','.')) # german decimal point (comma) replaced with dot 
+
+def extract_ingredients(recipe_text, recipe_title):
+    """Retrives the ingredients and respective amounts from a recipe written in latex. 
+
+    Args:
+        recipe_text (float): latex code of recipe
+        recipe_title (string): title of the recipe
+
+    Returns:
+        list: list of lists, each containing the recipe title, the ingredient, the part of the recipe it belongs to, the amount and the unit
+    """
+    ingredients = list()
+    recipe_ingredients_list = get_ingredients_list(recipe_text)
+    ingredient_section = ''
+    units = ['Tasse', 'Tassen', 'ml', 'mL', 'L', 'l', 'g', 'TL', 'EL', 'Dose', 'Prise', 'Stück', 'Spritzer', 'Handvoll', 'Würfel', 'Bund', 'Zweig', 'Packung']
+    for ingredient in recipe_ingredients_list:
+        ingredient = ingredient.strip('\t')
+        # sometimes the ingredients are not given for the enitre recipe but for sections
+        if 'multicolumn' in ingredient:
+            ingredient_section = ingredient.strip('}:\\')[ingredient.rfind('{'):]
+            continue
+
+        ing_name = ingredient.split('&')[-1]
+        ing_amount = ingredient.split('&')[0]
+
+        #clean-up of ingredient name
+        if 'textsuperscript' in ing_name:
+            ing_name = ing_name[:ing_name.find('\\textsuperscript')]
+
+        current_ingredient = [recipe_title, ing_name]
+        current_ingredient.append(ingredient_section)
+        
+        #do not add empty rows to the database
+        if ing_name=='':
+            continue
+        # the ingredient name can extend to the next row, at least sometimes signified by starting with 'oder '
+        elif ing_name[:5]=='oder ':
+            ingredients[-1][0] = f'{ingredients[-1][0]} {ing_name}' # this 2nd part of the ingredient name is added to the correct place
+            continue
+        # some recipes do not have any amount and are given in just one column
+        elif ing_amount==ing_name:
+            current_ingredient.append(float('NaN')) # amount
+            current_ingredient.append('') # unit
+        # some ingredients are not given with an amount
+        elif ing_amount=='' or ing_amount==' ':
+            current_ingredient.append(float('NaN')) #amount
+            current_ingredient.append('') #unit
+        # some ingredients are not listed with a specific amount
+        elif any(relative_amount in ing_amount for relative_amount in ['etwas', 'wenig', 'n.B.']):
+            current_ingredient.append(float('NaN')) #amount
+            current_ingredient.append('') #unit
+        # due to spacing issues the unit is sometimes in the 2nd column
+        elif '\\,' in ing_amount: # the non-breaking space signifies that the unit is in the first column with the amount
+            current_ingredient.append(extract_ingredient_amount(ing_amount.split('\\,')[0])) # amount
+            current_ingredient.append(ing_amount.split('\\,')[1].strip(' ')) #unit
+        # sometimes the unit is in the 1st column but no non-breaking space was used, here any non-numeric character is recognized as a unit
+        elif not is_string_float(ing_amount):
+            if is_string_float(ing_amount.split(' ')[0]):
+                current_ingredient.append(extract_ingredient_amount(ing_amount.split(' ')[0])) #amount
+                unit_included = True
+            elif  any(bindestrich in ing_amount for bindestrich in ['-', '–']):
+                current_ingredient.append(extract_ingredient_amount(ing_amount)) #amount
+                unit_included = False
+            else:
+                current_ingredient.append(1) #amount
+                logger.info(f'Check if this actually has the amount of 1: {ingredient}')
+                unit_included = True
+            if unit_included:
+                # sometimes more information ( as in 'kl. Dose') can be found
+                ing_unit = []
+                for el in ing_amount.split(' '):
+                    if not el.isnumeric():
+                        ing_unit.append(el)
+                current_ingredient.append(' '.join(ing_unit)) #unit
+            else:
+                current_ingredient.append('') #unit - if there is neither a non-breaking space or a normal space then there can't be a unit
+        # if the 1st column does not contain a unit, and the 2nd also does not contain a unit
+        elif ing_name.split(' ')[0] not in units:
+            current_ingredient.append(extract_ingredient_amount(ing_amount)) #amount
+            current_ingredient.append('') #unit
+        # find units in 2nd columns with the predefined list
+        elif ing_name.split(' ')[0] in units:
+            current_ingredient.append(extract_ingredient_amount(ing_amount)) #amount
+            current_ingredient.append(ing_name.split(' ')[0]) #unit
+            current_ingredient[1] == ' '.join(ing_name.split(' ')[1:]) # correct the name of the ingredient
+        else:
+            logger.warning(f'unable to import the following ingredient row: {ingredient}')
+        ingredients.append(current_ingredient)
+    return ingredients
