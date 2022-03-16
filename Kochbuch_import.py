16e95387c6ee87533f2d294acd9832ac4067d3df
Angelika Diehl <68091502+TruddelDu@users.noreply.github.com>
2022-03-16 17:31:48 +0100

    Import of full recipe-DB finallized
    finallized import functions of recipes and implemented import to MariaDB

--- Kochbuch_import.py
+++ Kochbuch_import.py
@@ -1,18 +1,27 @@
 import my_functions as mf
+from database_credentials import get_credentials
 import pandas as pd
 import logging
 import sys
 import time
-
+import sqlalchemy
+# from sqlalchemy.ext.declarative import declarative_base
 
 def main():
     path = r"D:\Benutzer\Angelika\Dokumente\DataScienceProjects\02_Kochbuch\Kochbuch\\"
     # First find the files of all recipes
     recipe_files = mf.find_recipe_files(f'{path}uebersicht.tex') 
 
-    # then extract the ingredients
-    replace_in_file = {'\\\\':'', '\\textonehalf':'0.5', '\\textonequarter':'0.25', 'Hand voll':'Handvoll'}
-    ingredients = [['Rezept', 'Zutat', 'Teil des Rezepts', 'Menge', 'Einheit',]]
+    # then import the files
+    replace_in_file = {
+        '\\textonehalf':'0.5', 
+        '\\textonequarter':'0.25', 
+        'Hand voll':'Handvoll', 
+        '\\textcelsius': '°C', 
+        '\\&':'und',
+        }
+    ingredients = [['RezeptID', 'Zutat', 'RezeptTeil', 'Menge', 'Einheit',]]
+    recipes = [['Name', 'StandardMenge', 'Quelle', 'Anweisungen']]
     for recipe_file in recipe_files:
         with open(f'{path}{recipe_file}.tex', encoding='utf-8') as recipe:
             # import latex and replace some annoying things
@@ -27,9 +36,64 @@ def main():
             # extract ingredients
             ingredients.extend(mf.extract_ingredients(recipe_text, recipe_title))
 
+            # extract recipe instructions
+            instructions_text , standard_portion_size = mf.extract_instructions(recipe_text)
+
+            # find the source 
+            source = mf.extract_source(recipe_text)
+            recipes.append([recipe_title, standard_portion_size, source, instructions_text])
             logger.info(f'import of {recipe_title} finished')
 
-    print(pd.DataFrame(ingredients[1:], columns=ingredients[0]))
+    # create and format recipes dataframe
+    recipes_df = pd.DataFrame(recipes[1:], columns=recipes[0])
+    # import tags of recipes (savory/sweet, kind of dish (main, side, cake, sweets), vegetarian/meat)
+    recipes_tags = pd.read_csv(r"D:\Benutzer\Angelika\Dokumente\DataScienceProjects\02_Kochbuch\Rezepte_Tags.csv",
+                                delimiter=';')
+    recipes = pd.merge(recipes_df, recipes_tags[['Name','herzhaft','Gerichtart','Küche','VeggieFleischart']], on ='Name')
+    
+    # create and format ingredients dataframe
+    ingredients = pd.DataFrame(ingredients[1:], columns=ingredients[0])
+    ingredients['RezeptID'] = ingredients['RezeptID'].replace(to_replace=dict(zip(recipes.Name,recipes.index)))
+    
+    # standardize amounts of ingredients to amounts per portion
+    ingredients = mf.standardize_amounts(ingredients, recipes)
+
+
+    
+    ### Connect to MariaDB and make tables
+    logger.info('Starting connection with MariaDB')
+    
+    # Credentials to database connection
+    hostname, dbname, uname, pwd = get_credentials()
+
+    engine = sqlalchemy.create_engine("mariadb+mariadbconnector://{user}:{pw}@{host}/{db}"
+				.format(host=hostname, db=dbname, user=uname, pw=pwd))
+
+    # Base.metadata.create_all(engine)
+
+    # Convert dataframe to sql table                                   
+    recipes.to_sql('recipes', engine, if_exists='fail', index=True, index_label='ID', 
+                    dtype = {   'ID':sqlalchemy.Integer(),
+                                'Name':sqlalchemy.String(length=75),
+                                'StandardMenge':sqlalchemy.Integer,
+                                'Quelle':sqlalchemy.String(length=300),
+                                'Anweisungen':sqlalchemy.String(length=3500),
+                                'herzhaft':sqlalchemy.Boolean(),
+                                'Gerichtart':sqlalchemy.String(length=15),
+                                'Küche':sqlalchemy.String(length=30),
+                                'VeggieFleischart':sqlalchemy.String(length=15),
+                    })
+
+    ingredients.to_sql('ingredients', engine, if_exists='fail', index=True, index_label='ID',
+                        dtype={     'ID':sqlalchemy.Integer(),
+                                    'RezeptID':sqlalchemy.Integer(),
+                                    'Zutat':sqlalchemy.String(length=40),
+                                    'RezeptTeil':sqlalchemy.String(length=40),
+                                    'Menge':sqlalchemy.Float(),
+                                    'Einheit':sqlalchemy.String(length=15),
+                        })
+    logger.info('tables created.')
+    return
 
 
 if __name__ == '__main__':
--- my_functions.py
+++ my_functions.py
@@ -1,5 +1,5 @@
 import numpy as np
-from re import split as resplit
+import re
 import logging
 
 logger = logging.getLogger(__name__)
@@ -33,7 +33,8 @@ def get_ingredients_list(recipe_text):
     #find the table and from there the next newline
     ingredients_start = recipe_text.find('begin{tabular}{l') + recipe_text[recipe_text.find('begin{tabular}{l')+1:].find('\n') + 1
     ingredients_end = recipe_text.find('end{tabular}')-1 # find the end of the table
-    return recipe_text[ingredients_start:ingredients_end].split('\n')
+    ingredients = recipe_text[ingredients_start:ingredients_end].replace('\\\\','' )
+    return ingredients.split('\n')
 
 def is_string_float(potential_float):
     """Returns True if the given string can be formatted into a float. Excepts dots and commas as decimal separators.
@@ -72,7 +73,7 @@ def extract_ingredient_amount(ing_amount):
         return float(ing_amount.split('/')[0])/float(ing_amount.split('/')[1])
     # sometimes a range is given. in that case reutrn the mean
     elif any(bindestrich in ing_amount for bindestrich in ['-', '–']):
-        parts = resplit('-|–', ing_amount.split('\\,')[0])
+        parts = re.split('-|–', ing_amount.split('\\,')[0])
         return np.mean([float(amount) for amount in parts])
     # elif any(word in ing_amount for word in ['etwas', 'wenig']):
     #     return float('NaN')
@@ -92,7 +93,7 @@ def extract_ingredients(recipe_text, recipe_title):
     ingredients = list()
     recipe_ingredients_list = get_ingredients_list(recipe_text)
     ingredient_section = ''
-    units = ['Tasse', 'Tassen', 'ml', 'mL', 'L', 'l', 'g', 'TL', 'EL', 'Dose', 'Prise', 'Stück', 'Spritzer', 'Handvoll', 'Würfel', 'Bund', 'Zweig', 'Packung']
+    units = ['Tasse', 'Tassen', 'ml', 'mL', 'L', 'l', 'g', 'TL', 'EL', 'Dose', 'Dosen','Prise', 'Stück', 'Spritzer', 'Handvoll', 'Würfel', 'Bund', 'Zweig', 'Packung']
     for ingredient in recipe_ingredients_list:
         ingredient = ingredient.strip('\t')
         # sometimes the ingredients are not given for the enitre recipe but for sections
@@ -100,7 +101,7 @@ def extract_ingredients(recipe_text, recipe_title):
             ingredient_section = ingredient.strip('}:\\')[ingredient.rfind('{'):]
             continue
 
-        ing_name = ingredient.split('&')[-1]
+        ing_name = ingredient.split('&')[-1].strip(' ')
         ing_amount = ingredient.split('&')[0]
 
         #clean-up of ingredient name
@@ -115,7 +116,15 @@ def extract_ingredients(recipe_text, recipe_title):
             continue
         # the ingredient name can extend to the next row, at least sometimes signified by starting with 'oder '
         elif ing_name[:5]=='oder ':
-            ingredients[-1][0] = f'{ingredients[-1][0]} {ing_name}' # this 2nd part of the ingredient name is added to the correct place
+            ingredients[-1][1] = f'{ingredients[-1][1]} {ing_name}' # this 2nd part of the ingredient name is added to the correct place
+            continue
+        # the ingredient name is extended into the next row and contained in brackets
+        elif (ing_name[0]=='(' and ing_name[-1]==')') and ing_amount.strip(' ')=='':
+            ingredients[-1][1] = f'{ingredients[-1][1]} {ing_name}' # this 2nd part of the ingredient name is added to the correct place
+            continue
+        # the ingredient is extended into the next row, catching single cases
+        elif ing_name in ['in Scheiben', 'Mandelgeschmack', 'grob gehackt', 'Dekorieren']:
+            ingredients[-1][1] = f'{ingredients[-1][1]} {ing_name}' # this 2nd part of the ingredient name is added to the correct place
             continue
         # some recipes do not have any amount and are given in just one column
         elif ing_amount==ing_name:
@@ -133,18 +142,22 @@ def extract_ingredients(recipe_text, recipe_title):
         elif '\\,' in ing_amount: # the non-breaking space signifies that the unit is in the first column with the amount
             current_ingredient.append(extract_ingredient_amount(ing_amount.split('\\,')[0])) # amount
             current_ingredient.append(ing_amount.split('\\,')[1].strip(' ')) #unit
-        # sometimes the unit is in the 1st column but no non-breaking space was used, here any non-numeric character is recognized as a unit
+        
         elif not is_string_float(ing_amount):
+            # sometimes the unit is in the 1st column but no non-breaking space was used, here any non-numeric character is recognized as a unit
             if is_string_float(ing_amount.split(' ')[0]):
                 current_ingredient.append(extract_ingredient_amount(ing_amount.split(' ')[0])) #amount
                 unit_included = True
+            # the amount is sometimes given as a range. here the average of the top and bottom borders is used
             elif  any(bindestrich in ing_amount for bindestrich in ['-', '–']):
                 current_ingredient.append(extract_ingredient_amount(ing_amount)) #amount
                 unit_included = False
+            # if none of the above is true, assume 1 to be the amount
             else:
                 current_ingredient.append(1) #amount
                 logger.info(f'Check if this actually has the amount of 1: {ingredient}')
                 unit_included = True
+            
             if unit_included:
                 # sometimes more information ( as in 'kl. Dose') can be found
                 ing_unit = []
@@ -166,4 +179,85 @@ def extract_ingredients(recipe_text, recipe_title):
         else:
             logger.warning(f'unable to import the following ingredient row: {ingredient}')
         ingredients.append(current_ingredient)
+    return ingredients
+
+def get_instruction_text(recipe_text):
+    """Returns recipe instructions with minimal cleaning.
+
+    Args:
+        recipe_text (sting): latex code of recipe
+
+    Returns:
+        string: recupe instructions
+    """
+    instructions_start = recipe_text.find('begin{minipage}[t][][t]{0.66') + 40
+    instructions_end = instructions_start + recipe_text[instructions_start:].find('end{minipage}') -2
+    instructions =  recipe_text[instructions_start:instructions_end]
+    #some recipes have long instructions covering 2 pages
+    if 'begin{minipage}[t!]{0.66' in recipe_text:
+        instructions_start = recipe_text.find('begin{minipage}[t!]{0.66') + 41
+        instructions_end = instructions_start + recipe_text[instructions_start:].find('end{minipage}') -2
+        instructions +=  recipe_text[instructions_start:instructions_end]
+    return instructions.replace('\\,', ' ').replace('\\\\', '\n')
+
+def extract_instructions(recipe_text):
+    """Returns the formatted recipe instructions and the standard portion size. 
+    Returns NaN for the latter if is not given in the recipe.
+
+    Args:
+        recipe_text (string): latex code of recipe
+
+    Returns:
+        tuple: instructions (sting), standard portion size (int)
+    """
+    instructions_text = get_instruction_text(recipe_text)
+    find_portion = re.compile('(\d+) (Portionen|Stück)', re.IGNORECASE)
+    # some recipes give the portionsize at the end of the instructions
+    if find_portion.search(instructions_text,-50): # returns None when nothing is found and doesn't trigger
+        portion_match = find_portion.search(instructions_text,-50)
+        # remove the portion size from the instructions
+        portion_start = portion_match.span()[0]
+        instructions_text = instructions_text[:instructions_text.rfind('\n', 0, portion_start)]
+        standard_portion_size = int(portion_match.group(0)[0])
+    else:
+        standard_portion_size = 1
+
+    return instructions_text, standard_portion_size
+
+def extract_source(recipe_text):
+    """Extracts the source of the recipe if it is given. Else returns None
+
+    Args:
+        recipe_text (string):  latex code of recipe
+
+    Returns:
+        string: source of the recipe (or None)
+    """
+    # find the part in the recipe where the source might be defined
+    source = recipe_text[recipe_text.rfind('end{minipage}')+13:]
+    # remove the formatting
+    formatting = ['\\vfill', '\\noindent', '\\textit{', '\\clearpage', '}', '\n', '\\\\', '{']
+    for format in formatting:
+        source = source.replace(format, '')
+    source = source.strip(' ')
+    #if there is something found in this part, return it as the source
+    if len(source)>3:# bigger 3 to ensure that any further whitespace chars are ignored
+        return source
+    else:
+         return None
+
+
+    
+def standardize_amounts(ingredients, recipes):
+    """Standardizes the amounts given per ingredient to the amount per portion.
+
+    Args:
+        ingredients (_type_): ingredients dataframe (not standardized)
+        recipes (_type_): recipes dataframe
+
+    Returns:
+        _type_: standardized amounts column of ingredients dataframe
+    """
+    ingredients['Menge'] = [amount/recipes.iloc[id]['StandardMenge'] for amount, id in zip(ingredients['Menge'], ingredients['RezeptID'])]
+    
     return ingredients
